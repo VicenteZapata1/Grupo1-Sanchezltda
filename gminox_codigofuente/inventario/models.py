28d20ab266890eb79a6539a4d00d1c4a8ec283d2
VicenteZapata1 <49206109+VicenteZapata1@users.noreply.github.com>
2019-09-11 16:27:20 -0300

    Add files via upload

--- gminox_codigofuente/inventario/models.py
+++ gminox_codigofuente/inventario/models.py
@@ -11,3 +11,36 @@ class Material(models.Model):
 
     def __str__(self):
         return "{}".format(self.nombre)
+
+class EPP(models.Model):
+    nombre=models.CharField(max_length=60)
+    cantidad=models.IntegerField()
+    epps=models.Manager()
+    def __str__(self):
+        return "{}".format(self.nombre)
+
+class Herramienta(models.Model):
+    nombre=models.CharField(max_length=60)
+    cantidad=models.IntegerField()
+    herramientas=models.Manager()
+    def __str__(self):
+        return "{}".format(self.nombre)      
+
+class Insumo(models.Model):
+    nombre=models.CharField(max_length=60)
+    medida=models.FloatField()
+    cantidad=models.IntegerField()
+    insumos=models.Manager()
+    def __str__(self):
+        return "{}".format(self.nombre)          
+
+class Despunte(models.Model):
+    nombre=models.CharField(max_length=60)
+    cantidad=models.IntegerField()
+    largo=models.IntegerField()
+    ancho=models.IntegerField()
+    espesor=models.IntegerField()
+    despuntes=models.Manager()
+
+    def __str__(self):
+        return "{}".format(self.nombre)
--- gminox_codigofuente/inventario/views.py
+++ gminox_codigofuente/inventario/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import Material #, EPP, Herramienta, Insumo
+from .models import Material,EPP,Herramienta,Insumo,Despunte
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -29,3 +29,100 @@ class MaterialDelete(DeleteView):
     model = Material
     template_name='./material_form.html'
     success_url = reverse_lazy('materiales')        
+
+ 
+class HomeEPPView(LoginRequiredMixin,TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'epps.html',{'epps': EPP.epps.all()})
+
+class DetalleEPPView(LoginRequiredMixin,TemplateView):
+    def get(self, request, **kwargs):
+        id=kwargs["id"]
+        return render(request, 'epp.html', {'epp': EPP.epps.get(id=id)})   
+
+class EPPCreate(CreateView):
+    model = EPP
+    template_name='./epp_form.html'
+    fields = '__all__'
+
+class EPPUpdate(UpdateView):
+    model = EPP
+    template_name='./epp_form.html'
+    fields = ['nombre','cantidad']
+
+class EPPDelete(DeleteView):
+    model = EPP
+    template_name='./epp_form.html'
+    success_url = reverse_lazy('epps')
+
+class HomeHerramientasView(LoginRequiredMixin,TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'herramientas.html',{'herramientas': Herramienta.herramientas.all()})
+
+class DetalleHerramientaView(LoginRequiredMixin,TemplateView):
+    def get(self, request, **kwargs):
+        id=kwargs["id"]
+        return render(request, 'herramienta.html', {'herramienta': Herramienta.herramientas.get(id=id)})
+
+class HerramientaCreate(CreateView):
+    model = Herramienta
+    template_name='./herramienta_form.html'
+    fields = '__all__'
+
+class HerramientaUpdate(UpdateView):
+    model = Herramienta
+    template_name='./herramienta_form.html'
+    fields = ['nombre','cantidad']
+
+class HerramientaDelete(DeleteView):
+    model = Herramienta
+    template_name='./herramienta_form.html'
+    success_url = reverse_lazy('herramientas')
+
+class HomeInsumosView(LoginRequiredMixin,TemplateView):
+    def get(self,request, **kwargs):
+        return render(request, 'insumos.html',{'insumos':Insumo.insumos.all()})
+
+class DetalleInsumoView(LoginRequiredMixin,TemplateView):
+    def get(self, request, **kwargs):
+        id=kwargs["id"]
+        return render(request, 'insumo.html', {'insumo': Insumo.insumos.get(id=id)})
+
+class InsumoCreate(CreateView):
+    model = Insumo
+    template_name='./insumo_form.html'
+    fields = '__all__'
+
+class InsumoUpdate(UpdateView):
+    model = Insumo
+    template_name='./insumo_form.html'
+    fields = ['nombre','medida','cantidad']    
+
+class InsumoDelete(DeleteView):
+    model = Insumo
+    template_name='./insumo_form.html'
+    success_url = reverse_lazy('insumos')
+
+class HomeDespuntesView(LoginRequiredMixin,TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'despuntes.html', {'despuntes': Despunte.despuntes.all()})
+
+class DetalleDespunteView(LoginRequiredMixin,TemplateView):
+    def get(self, request, **kwargs):
+        id=kwargs["id"]
+        return render(request, 'despunte.html', {'despunte': Despunte.despuntes.get(id=id)})
+
+class DespunteCreate(CreateView):
+    model = Despunte
+    template_name='./despunte_form.html'
+    fields = '__all__'
+
+class DespunteUpdate(UpdateView):
+    model = Despunte
+    template_name='./despunte_form.html'
+    fields = ['nombre','cantidad','largo','ancho','espesor']
+
+class DespunteDelete(DeleteView):
+    model = Despunte
+    template_name='./despunte_form.html'
+    success_url = reverse_lazy('despuntes')        
