e30865315a5f21957c72bcd0c038835f60d086da
FelipeBass <felipejaque97@hotmail.com>
2019-11-25 18:04:12 -0300

    todos los permisos listos, falta lo de moreno

--- gminox_codigofuente/Cotizacion/views.py
+++ gminox_codigofuente/Cotizacion/views.py
@@ -4,16 +4,21 @@ from django.core.files.storage import FileSystemStorage
 from django.urls import reverse_lazy
 from .forms import CotizacionForm
 from .models import CotizacionGminox
-
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
 
+def test_func(request):
+    return request.groups.filter(name="Oficina").exists()
 
 class Upload_PDF(TemplateView):
     template_name ='upload_PDF'
 
-
+@user_passes_test(test_func)
 def upload(request):
      context = {}
      if request.method == 'POST':
@@ -22,13 +27,13 @@ def upload(request):
         name = fs.save(uploaded_file.name, uploaded_file)
         context['url'] = fs.url(name)
      return render(request, 'Cotizacion/upload.html', context)
-
+@user_passes_test(test_func)
 def cotizacion_list(request):
     cotizaciones =CotizacionGminox.objects.all( )
     return render(request, 'Cotizacion/cotizacion_list.html', {
         'cotizaciones': cotizaciones
     })
-
+@user_passes_test(test_func)
 def upload_cotizacion(request):
     if request.method == 'POST': 
         form = CotizacionForm(request.POST, request.FILES)
--- gminox_codigofuente/facturas/views.py
+++ gminox_codigofuente/facturas/views.py
@@ -6,13 +6,22 @@ from .forms import FacturaCompraForm
 from .forms import FacturaVentaForm
 from .models import FacturaCompraGminox
 from .models import FacturaVentaGminox
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
 
 # Create your views here.
+
+def test_func(request):
+    return request.groups.filter(name="Oficina").exists()
+
+        
 class Upload_PDF(TemplateView):
     template_name ='upload_PDF'
 
-
+@user_passes_test(test_func)
 def upload(request):
      context = {}
      if request.method == 'POST':
@@ -22,12 +31,14 @@ def upload(request):
         context['url'] = fs.url(name)
      return render(request, 'facturas/upload.html', context)
 
+@user_passes_test(test_func)
 def facturacompra_list(request):
     facturascompra = FacturaCompraGminox.objects.all( )
     return render(request, 'facturas/facturacompra_list.html', {
         'facturascompra': facturascompra
     })
 
+@user_passes_test(test_func)
 def upload_facturacompra(request):
     if request.method == 'POST': 
         form = FacturaCompraForm(request.POST, request.FILES)
@@ -40,6 +51,7 @@ def upload_facturacompra(request):
         'form': form
     })
 
+@user_passes_test(test_func)
 def uploadVenta(request):
      context = {}
      if request.method == 'POST':
@@ -49,12 +61,14 @@ def uploadVenta(request):
         context['url'] = fs.url(name)
      return render(request, 'facturas/uploadventa.html', context)
 
+@user_passes_test(test_func)
 def facturaventa_list(request):
     facturasventa = FacturaVentaGminox.objects.all( )
     return render(request, 'facturas/facturaventa_list.html', {
         'facturasventa': facturasventa
     })
 
+@user_passes_test(test_func)
 def upload_facturaventa(request):
     if request.method == 'POST': 
         form2 = FacturaVentaForm(request.POST, request.FILES)
